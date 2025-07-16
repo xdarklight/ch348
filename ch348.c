da7a035c49c6714a3082d97e896f262801007790
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2025-07-16 14:23:50 +0200

    ch348: move urb submission / killing back to attach/detach for now
    This is an attempt at fixing:
    [   26.411521] refcount_t: addition on 0; use-after-free.
    [   26.416644] WARNING: CPU: 3 PID: 427 at lib/refcount.c:25 refcount_warn_saturate+0x120/0x144
    [   26.424961] Modules linked in: 8021q garp mrp stp llc ipv6 meson_gxl smsc dwmac_generic onboard_usb_dev dwmac_meson8b stmmac_platform meson_canvas mdio_mux_meson_gxl stmmac meson_gxbb_wdt amlogic_gxl_crypto pcs_xpcs meson_ir rc_core crypto_engine nvmem_meson_efuse
    [   26.448164] CPU: 3 UID: 0 PID: 427 Comm: python3 Not tainted 6.15.7-00001-g62df3865f5f5 #49 PREEMPT
    [   26.457216] Hardware name: Libre Computer AML-S905X-CC (DT)
    [   26.462737] pstate: 60000005 (nZCv daif -PAN -UAO -TCO -DIT -SSBS BTYPE=--)
    [   26.469636] pc : refcount_warn_saturate+0x120/0x144
    [   26.474466] lr : refcount_warn_saturate+0x120/0x144
    [   26.479296] sp : ffff800080cb3930
    [   26.482573] x29: ffff800080cb3930 x28: ffff18307a0dd9e0 x27: 0000000000020902
    [   26.489646] x26: ffffd1aab3683278 x25: ffffd1aab3682e48 x24: ffff183001e28000
    [   26.496719] x23: ffff18300dafbf00 x22: ffff183001aecdf8 x21: ffff183079927800
    [   26.503791] x20: ffff183001aecc00 x19: ffff183001e28000 x18: 0000000000000006
    [   26.510864] x17: 000051aa27632000 x16: ffff80008058d000 x15: ffff800080cb34ff
    [   26.517936] x14: 0000000000000000 x13: 2e656572662d7265 x12: ffffd1aab4126fb8
    [   26.525009] x11: 0000000000000058 x10: 0000000000000018 x9 : ffffd1aab4126fb8
    [   26.532081] x8 : 0000000000000157 x7 : ffffd1aab417efb8 x6 : ffffd1aab417efb8
    [   26.539153] x5 : 0000000000017fe8 x4 : 0000000000000000 x3 : 0000000000000000
    [   26.546226] x2 : 0000000000000000 x1 : 0000000000000000 x0 : ffff1830012be900
    [   26.553299] Call trace:
    [   26.555715]  refcount_warn_saturate+0x120/0x144 (P)
    [   26.560544]  ch348_open+0x130/0x258
    [   26.563994]  serial_port_activate+0x8c/0xf4
    [   26.568134]  tty_port_open+0x8c/0x140
    [   26.571756]  serial_open+0x20/0x2c
    [   26.575120]  tty_open+0x134/0x6a0
    [   26.578397]  chrdev_open+0xb0/0x21c
    [   26.581848]  do_dentry_open+0x154/0x610
    [   26.585642]  vfs_open+0x2c/0xe8
    [   26.588748]  path_openat+0x700/0x1090
    [   26.592370]  do_filp_open+0xa8/0x164
    [   26.595906]  do_sys_openat2+0x94/0x104
    [   26.599615]  __arm64_sys_openat+0x64/0xc0
    [   26.603582]  invoke_syscall+0x48/0x104
    [   26.607291]  el0_svc_common.constprop.0+0x40/0xe0
    [   26.611949]  do_el0_svc+0x1c/0x28
    [   26.615226]  el0_svc+0x30/0xcc
    [   26.618245]  el0t_64_sync_handler+0x10c/0x138
    [   26.622557]  el0t_64_sync+0x198/0x19c
    [   26.626181] ---[ end trace 0000000000000000 ]---
    [   26.632487] ------------[ cut here ]------------
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

--- ch348.c
+++ ch348.c
@@ -17,7 +17,6 @@
 #include <linux/kernel.h>
 #include <linux/kfifo.h>
 #include <linux/module.h>
-#include <linux/mutex.h>
 #include <linux/refcount.h>
 #include <linux/serial.h>
 #include <linux/serial_reg.h>
@@ -126,8 +125,6 @@ struct ch348_port {
  * @write_work:		worker for processing the write queues
  * @tx_ep:		endpoint number for serial data transmit/write operation
  * @config_ep:		endpoint number for configure operations
- * @num_open_ports:	number of ports currently open ports
- * @manage_urbs_lock:	protects submitting / killing URBs across all ports
  * @package_type:	indicates package type
  */
 struct ch348 {
@@ -138,9 +135,6 @@ struct ch348 {
 
 	int tx_ep;
 	int config_ep;
-
-	refcount_t num_open_ports;
-	struct mutex manage_urbs_lock;
 
 	enum ch348_package package_type;
 };
@@ -179,20 +173,14 @@ struct ch348_status_entry {
 
 static int ch348_submit_urbs(struct usb_serial *serial)
 {
-	struct ch348 *ch348 = usb_get_serial_data(serial);
-	int ret = 0;
-
-	mutex_lock(&ch348->manage_urbs_lock);
-
-	if (refcount_read(&ch348->num_open_ports))
-		goto out_increment_refcount;
+	int ret;
 
 	ret = usb_serial_generic_open(NULL,
 				      serial->port[CH348_PORTNUM_SERIAL_RX_TX]);
 	if (ret) {
 		dev_err(&serial->dev->dev, "Failed to open RX/TX port: %d\n",
 			ret);
-		goto out_unlock;
+		return ret;
 	}
 
 	ret = usb_serial_generic_open(NULL,
@@ -201,28 +189,16 @@ static int ch348_submit_urbs(struct usb_serial *serial)
 		dev_err(&serial->dev->dev,
 			"Failed to submit STATUS/INT URB: %d\n", ret);
 		usb_serial_generic_close(serial->port[CH348_PORTNUM_SERIAL_RX_TX]);
-		goto out_unlock;
+		return ret;
 	}
 
-out_increment_refcount:
-	refcount_inc(&ch348->num_open_ports);
-
-out_unlock:
-	mutex_unlock(&ch348->manage_urbs_lock);
-
-	return ret;
+	return 0;
 }
 
 static void ch348_kill_urbs(struct usb_serial *serial)
 {
-	struct ch348 *ch348 = usb_get_serial_data(serial);
-
-	if (refcount_dec_and_mutex_lock(&ch348->num_open_ports,
-					&ch348->manage_urbs_lock)) {
-		usb_serial_generic_close(serial->port[CH348_PORTNUM_STATUS_INT_CONFIG]);
-		usb_serial_generic_close(serial->port[CH348_PORTNUM_SERIAL_RX_TX]);
-		mutex_unlock(&ch348->manage_urbs_lock);
-	}
+	usb_serial_generic_close(serial->port[CH348_PORTNUM_STATUS_INT_CONFIG]);
+	usb_serial_generic_close(serial->port[CH348_PORTNUM_SERIAL_RX_TX]);
 }
 
 static void ch348_write_done(struct usb_serial_port *port)
@@ -567,10 +543,6 @@ static int ch348_open(struct tty_struct *tty, struct usb_serial_port *port)
 	timer_setup(&ch348->ports[port->port_number].tx_timeout,
 		    ch348_tx_timeout, 0);
 
-	ret = ch348_submit_urbs(port->serial);
-	if (ret)
-		return ret;
-
 	if (tty)
 		ch348_set_termios(tty, port, NULL);
 
@@ -578,21 +550,17 @@ static int ch348_open(struct tty_struct *tty, struct usb_serial_port *port)
 	if (ret) {
 		dev_err(&port->serial->dev->dev,
 			"Failed to configure R_C2_ACTIVATE: %d\n", ret);
-		goto err_kill_urbs;
+		return ret;
 	}
 
 	ret = ch348_port_config(port, CMD_W_R, R_C4, R_C4_ACTIVATE);
 	if (ret) {
 		dev_err(&port->serial->dev->dev,
 			"Failed to configure R_C4_ACTIVATE: %d\n", ret);
-		goto err_kill_urbs;
+		return ret;
 	}
 
 	return 0;
-
-err_kill_urbs:
-	ch348_kill_urbs(port->serial);
-	return ret;
 }
 
 static void ch348_close(struct usb_serial_port *port)
@@ -605,8 +573,6 @@ static void ch348_close(struct usb_serial_port *port)
 	spin_unlock_irqrestore(&port->lock, flags);
 
 	timer_shutdown_sync(&ch348->ports[port->port_number].tx_timeout);
-
-	ch348_kill_urbs(port->serial);
 }
 
 static int ch348_detect_version(struct usb_serial *serial)
@@ -665,9 +631,11 @@ static int ch348_attach(struct usb_serial *serial)
 	ch348->config_ep = usb_sndbulkpipe(serial->dev,
 					   config_port->bulk_out_endpointAddress);
 
-	mutex_init(&ch348->manage_urbs_lock);
-
 	ret = ch348_detect_version(serial);
+	if (ret)
+		goto err_free_ch348;
+
+	ret = ch348_submit_urbs(serial);
 	if (ret)
 		goto err_free_ch348;
 
@@ -683,6 +651,7 @@ static void ch348_release(struct usb_serial *serial)
 	struct ch348 *ch348 = usb_get_serial_data(serial);
 
 	cancel_work_sync(&ch348->write_work);
+	ch348_kill_urbs(serial);
 
 	kfree(ch348);
 }
