xdarklight/ch348

commit e56532bae050bae0c36b6ec1be8b49380d327a7a

ch348: clear write state in all cases
All errors in ch348_write_bulk_callback() need to trigger it as well as
we need to trigger it during suspend. The goal is that once we know that
the write URB is no longer pending, we clear the state so the next write
submission can set it again without getting stuck forever.

Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

ch348.c
@@ -210,6 +210,14 @@ static void ch348_kill_read_urbs(struct usb_serial *serial)
 	ch348_kill_port_read_urbs(serial->port[CH348_PORTNUM_SERIAL_RX]);
 }
 
+static void ch348_clear_write_state(struct usb_serial_port *port)
+{
+	scoped_guard(spinlock_irqsave, &port->lock)
+		port->tx_bytes = 0;
+
+	clear_bit_unlock(USB_SERIAL_WRITE_BUSY, &port->flags);
+}
+
 static int ch348_write_start(struct usb_serial_port *port, gfp_t mem_flags)
 {
 	struct ch348_txbuf *txb;
@@ -249,11 +257,7 @@ static int ch348_write_start(struct usb_serial_port *port, gfp_t mem_flags)
 	ret = usb_submit_urb(port->write_urb, mem_flags);
 	if (ret) {
 		dev_err_console(port, "Failed to submit write URB: %d\n", ret);
-
-		scoped_guard(spinlock_irqsave, &port->lock)
-			port->tx_bytes -= tx_bytes;
-
-		clear_bit_unlock(USB_SERIAL_WRITE_BUSY, &port->flags);
+		ch348_clear_write_state(port);
 	}
 
 	return ret;
@@ -261,12 +265,10 @@ static int ch348_write_start(struct usb_serial_port *port, gfp_t mem_flags)
 
 static void ch348_write_done(struct usb_serial_port *port)
 {
-	scoped_guard(spinlock_irqsave, &port->lock) {
+	scoped_guard(spinlock_irqsave, &port->lock)
 		port->icount.tx += port->tx_bytes;
-		port->tx_bytes = 0;
-	}
 
-	clear_bit_unlock(USB_SERIAL_WRITE_BUSY, &port->flags);
+	ch348_clear_write_state(port);
 
 	ch348_write_start(port, GFP_ATOMIC);
 	usb_serial_port_softint(port);
@@ -396,7 +398,7 @@ static void ch348_write_bulk_callback(struct urb *urb)
 	switch (urb->status) {
 	case 0:
 		/* processing continues once we receive UART_IIR_THRI. */
-		break;
+		return;
 	case -ECONNRESET:
 	case -ENOENT:
 	case -ESHUTDOWN:
@@ -409,9 +411,11 @@ static void ch348_write_bulk_callback(struct urb *urb)
 		dev_err_console(port,
 				"ch348_write_bulk_callback - nonzero write bulk status received: %d\n",
 				urb->status);
-		ch348_write_done(port);
 		break;
 	}
+
+	ch348_clear_write_state(port);
+	usb_serial_port_softint(port);
 }
 
 static int ch348_write_config(struct usb_serial *serial, u8 cmd, u8 reg,
@@ -678,13 +682,6 @@ static void ch348_close(struct usb_serial_port *port)
 	if (ret)
 		dev_dbg(&port->dev, "Failed to disable interrupts: %d\n", ret);
 
-	usb_kill_urb(port->write_urb);
-
-	scoped_guard(spinlock_irqsave, &port->lock) {
-		kfifo_reset_out(&port->write_fifo);
-		port->tx_bytes = 0;
-	}
-
 	scoped_guard(mutex, &ch348->open_ports_lock) {
 		clear_bit(port->port_number, ch348->open_ports);
 
@@ -692,7 +689,12 @@ static void ch348_close(struct usb_serial_port *port)
 			ch348_kill_read_urbs(port->serial);
 	}
 
-	clear_bit_unlock(USB_SERIAL_WRITE_BUSY, &port->flags);
+	scoped_guard(spinlock_irqsave, &port->lock)
+		kfifo_reset_out(&port->write_fifo);
+
+	ch348_clear_write_state(port);
+
+	usb_kill_urb(port->write_urb);
 }
 
 static int ch348_detect_version(struct usb_serial *serial)
@@ -772,6 +774,19 @@ static int ch348_calc_num_ports(struct usb_serial *serial,
 	return CH348_MAXPORT;
 }
 
+static int ch348_suspend(struct usb_serial *serial, pm_message_t message)
+{
+	struct ch348 *ch348 = usb_get_serial_data(serial);
+	unsigned int i;
+
+	scoped_guard(mutex, &ch348->open_ports_lock) {
+		for_each_set_bit(i, ch348->open_ports, CH348_MAXPORT)
+			ch348_clear_write_state(serial->port[i]);
+	}
+
+	return 0;
+}
+
 static int ch348_resume(struct usb_serial *serial)
 {
 	struct ch348 *ch348 = usb_get_serial_data(serial);
@@ -826,6 +841,7 @@ static struct usb_serial_driver ch348_device = {
 	.calc_num_ports =	ch348_calc_num_ports,
 	.attach =		ch348_attach,
 	.release =		ch348_release,
+	.suspend =		ch348_suspend,
 	.resume =		ch348_resume,
 };
 

commit 65803438aeab58c330e17ed1775ba26a82996da3

ch348: quiesce writes before clearing suspend state
The usb-serial core calls the driver suspend callback before poisoning
the port URBs. Clearing WRITE_BUSY from the callback can therefore race
with an active or newly submitted write URB and leave the queue state
inconsistent after resume.

Do not submit new writes once the core has marked the serial device
suspended. Kill each active write URB before clearing its state so
queued data can be restarted safely by resume.

Fixes: e56532bae050 ("ch348: clear write state in all cases")
Signed-off-by: Gleb Pesin <g.pesin@globalte.ru>

ch348.c
@@ -228,6 +228,9 @@ static int ch348_write_start(struct usb_serial_port *port, gfp_t mem_flags)
 	unsigned int tx_bytes;
 	int ret;
 
+	if (READ_ONCE(port->serial->suspend_count))
+		return 0;
+
 	if (test_and_set_bit_lock(USB_SERIAL_WRITE_BUSY, &port->flags))
 		return 0;
 
@@ -794,8 +797,10 @@ static int ch348_suspend(struct usb_serial *serial, pm_message_t message)
 	unsigned int i;
 
 	scoped_guard(mutex, &ch348->open_ports_lock) {
-		for_each_set_bit(i, ch348->open_ports, CH348_MAXPORT)
+		for_each_set_bit(i, ch348->open_ports, CH348_MAXPORT) {
+			usb_kill_urb(serial->port[i]->write_urb);
 			ch348_clear_write_state(serial->port[i]);
+		}
 	}
 
 	return 0;