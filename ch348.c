xdarklight/ch348

commit afd151ace5e517427967a374461134f25c18ffaf

ch348: correct stop bit configuration in struct ch348_config_data_init
The vendor driver uses:
  newline.bCharFormat = termios->c_cflag & CSTOPB ? 2 : 1;

Apply the same logic and drop the previously used constants in favour of
using 1 and 2 (same approach as for databits and paritytype).

Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

ch348.c
@@ -136,15 +136,12 @@ struct ch348_config_buf {
 struct ch348_config_data_init {
 	u8 port;
 	__be32 baudrate;
-	u8 format;
+	u8 stopbits;
 	u8 paritytype;
 	u8 databits;
 	u8 rate;
 	u8 unknown;
 } __packed;
-
-#define CH348_CONFIG_DATA_INIT_FORMAT_ONE_STOPBIT	0x2
-#define CH348_CONFIG_DATA_INIT_FORMAT_TWO_STOPBITS	0x0
 
 struct ch348_ven_r_msr {
 	u8 control;
@@ -585,9 +582,9 @@ static void ch348_set_termios(struct tty_struct *tty, struct usb_serial_port *po
 	config.baudrate = cpu_to_be32(baudrate);
 
 	if (termios->c_cflag & CSTOPB)
-		config.format = CH348_CONFIG_DATA_INIT_FORMAT_ONE_STOPBIT;
+		config.stopbits = 2;
 	else
-		config.format = CH348_CONFIG_DATA_INIT_FORMAT_TWO_STOPBITS;
+		config.stopbits = 1;
 
 	config.rate = max_t(speed_t, 5, (10000 * 15 / baudrate) + 1);
 

commit deb7912509f9b67ba01cbefc6de32874046fd285

ch348: fix stop-bit command encoding
The CH348 command format uses 0x00 for one stop bit and 0x02 for
two stop bits. The current code passes the logical stop-bit count
instead, causing one-stop-bit configurations to be sent as the
unsupported value 0x01.

Use named constants for the values expected by the device.

Fixes: afd151ace5e5 ("ch348: correct stop bit configuration in struct ch348_config_data_init")
Signed-off-by: Gleb Pesin <g.pesin@globalte.ru>

ch348.c
@@ -144,6 +144,9 @@ struct ch348_config_data_init {
 	u8 unknown;
 } __packed;
 
+#define CH348_CONFIG_STOP_BITS_ONE	0x00
+#define CH348_CONFIG_STOP_BITS_TWO	0x02
+
 struct ch348_ven_r_msr {
 	u8 control;
 	u8 msr;
@@ -583,9 +586,9 @@ static void ch348_set_termios(struct tty_struct *tty, struct usb_serial_port *po
 	config.baudrate = cpu_to_be32(baudrate);
 
 	if (termios->c_cflag & CSTOPB)
-		config.stopbits = 2;
+		config.stopbits = CH348_CONFIG_STOP_BITS_TWO;
 	else
-		config.stopbits = 1;
+		config.stopbits = CH348_CONFIG_STOP_BITS_ONE;
 
 	config.rate = max_t(speed_t, 5, (10000 * 15 / baudrate) + 1);
 